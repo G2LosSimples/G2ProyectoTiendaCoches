8debebb00f8316a7defcebb898b7175699afe56e
Jairo <jairogarciachaves@gmail.com>
2021-03-03 14:48:29 +0100

    Seguimos con el testing

--- models/CarModel.test.js
+++ models/CarModel.test.js
@@ -32,9 +32,49 @@ describe ('Car Model', ()=>{
         expect(validCar.costPrice).toBe(carData.costPrice);
         expect(validCar.leasingPrice).toBe(carData.leasingPrice);
         expect(validCar.leaseDate).toBe(carData.leaseDate);
-        // expect(validCar.carBrand).toBe(carData.carBrand);
-        // expect(validCar.carBrand).toBe(carData.carBrand);
+        expect(validCar.leaseReturnDate).toBe(carData.leaseReturnDate);
+        expect(validCar.carImage).toBe(carData.carImage);
+        expect(validCar.carColor).toBe(carData.carColor);
+        expect(validCar.seatsNumber).toBe(carData.seatsNumber);
+        expect(validCar.doorNumber).toBe(carData.doorNumber);
+        expect(validCar.trunkSize).toBe(carData.trunkSize);
+        expect(validCar.co2Emissions).toBe(carData.co2Emissions);
+        expect(validCar.carType).toBe(carData.carType);
+        expect(validCar.airConditioner).toBe(carData.airConditioner);
+        expect(validCar.transmissionType).toBe(carData.transmissionType);
+        expect(validCar.motorType).toBe(carData.motorType);
+        expect(validCar.stock).toBe(carData.stock);
+        expect(validCar.availability).toBe(carData.availability);
+        expect(validCar.transactionType).toBe(carData.transactionType);
     })
+
+    it('should insert a car succesfully but with a field not defined in model', async () => {
+
+        const carData = {carBrand:'ford', carModel:'mondeo', modelYear:'1999', nextItvDate:new Date("2021-02-21T00:00:00.000+00:00"), sellingPrice:2000, costPrice:500, leasingPrice:300, leaseDate:new Date("2021-02-21T00:00:00.000+00:00"), leaseReturnDate:new Date("2021-02-21T00:00:00.000+00:00"), carImage:'adsdas', carColor:'plateado', seatsNumber:5, doorNumber:5, trunkSize:50, co2Emissions:'asdassd', carType:'asdas', airConditioner:'NO', transmissionType:'auto', motorType:'gasolina', stock:2, availability:'SI', transactionType:'asssd', lolailo: 'lolailo' }
+
+        const validCar = await createCar(carData);
+
+        expect(validCar._id).toBeDefined();
+        expect(validCar.lolailo).toBeUndefined();
+        
+
+    })
+
+    it('should throw an error without a required field', async () => {
+
+        const carData = {carBrand:'ford'}
+        let err;
+        try{
+
+            const invalidCar = await createCar(carData);
+            error = invalidCar
+
+        }catch(error){ err = error }
+
+        expect(err.errors.carModel).toBeDefined();
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+
+    }) 
 
     afterAll(done => {
         mongoose.connection.close()
