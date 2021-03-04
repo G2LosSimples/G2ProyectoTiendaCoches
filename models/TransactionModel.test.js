dc5965c82d7cd64421e2c1eb302b0b3a996bede3
Jairo <jairogarciachaves@gmail.com>
2021-03-04 13:19:45 +0100

    Mas test transaction model
    transaction model read

--- models/TransactionModel.test.js
+++ models/TransactionModel.test.js
@@ -1,23 +1,62 @@
 const mongoose = require('mongoose');
-const {createTransaction}=require('../controllers/carSale/transactionFunctions');
+const {createTransaction,getTransactions, getSingleTransaction}=require('../controllers/carSale/transactionFunctions');
 const path = require('../globalConfig.json')
 
 describe('Transaction model',()=>{
-
-
+    
+    
     beforeAll(async () => {
         await mongoose.connect(path.mongoUri,
             { useUnifiedTopology: true,
                 useNewUrlParser: true,
                 useCreateIndex: true,
                 useFindAndModify: false }, (err) => {
-            if (err) {
-                console.error(err);
-                process.exit(1);
-            }
-        });
-    });
+                    if (err) {
+                        console.error(err);
+                        process.exit(1);
+                    }
+                });
+            });
 
+
+            describe ('Transaction Model Read', () => {
+
+                it('should return the correct number of transaction succesfully', async () => {
+        
+                const transactionData = {userId: '1', carId: 'asrgkñbjasd', total: 234, benefit: 123}
+                let validTransaction = createTransaction(transactionData);
+                validTransaction = await validTransaction.save();
+                const transactionList = await getTransactions();
+        
+                expect(transactionList).toHaveLength(1);
+        
+        
+                })
+
+                it('should return the transaction you are looking for', async () => {
+
+                const transactionData = {userId: '1', carId: 'asrgkñbjasd', total: 234, benefit: 123}
+                let validTransaction = createTransaction(transactionData);
+                validTransaction = await validTransaction.save();
+                const singleTransaction = await getSingleTransaction({_id:validTransaction._id});
+                
+                expect(singleTransaction._id).toStrictEqual(validTransaction._id);
+                expect(singleTransaction.userId).toBe(validTransaction.userId);
+                expect(singleTransaction.carId).toBe(validTransaction.carId);
+                expect(singleTransaction.total).toBe(validTransaction.total);
+                expect(singleTransaction.benefit).toBe(validTransaction.benefit);
+                expect(singleTransaction.transactionDate).toStrictEqual(validTransaction.transactionDate);
+
+
+
+                })
+        
+            })
+
+
+
+
+            
     it('should create a Transaction succesfully', async ()=> {
         const transactionData = {userId: '1', carId: 'asrgkñbjasd', total: 234, benefit: 123}
         let validTransaction = createTransaction(transactionData);
@@ -61,10 +100,15 @@ describe('Transaction model',()=>{
 
     })
 
+    
+
     afterAll(done => {
         mongoose.connection.close()
         done();
     })
 
+    
 
 })
+
+
